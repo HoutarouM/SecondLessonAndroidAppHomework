8e9e5b6ebaa86b42cd018f9b9cd3d304c0a94822
Marian Horoshko <marian.horoshko@gmail.com>
2022-10-08 22:01:08 +0200

    Add buttons that change background color

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.TextureView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,5 +34,37 @@ public class MainActivity extends AppCompatActivity {
         textSize -= textSize / 1.1f;
 
         textView.setTextSize(textSize);
+    }
+
+    public void changeTextColorOnRed(View view) {
+        Button b = (Button) findViewById(R.id.button_red);
+
+        Drawable color = (Drawable) b.getBackground();
+
+        findViewById(R.id.background).setBackground(color);
+    }
+
+    public void changeTextColorOnBlue(View view) {
+        Button b = (Button) findViewById(R.id.button_blue);
+
+        Drawable color = (Drawable) b.getBackground();
+
+        findViewById(R.id.background).setBackground(color);
+    }
+
+    public void changeTextColorOnYellow(View view) {
+        Button b = (Button) findViewById(R.id.button_yellow);
+
+        Drawable color = (Drawable) b.getBackground();
+
+        findViewById(R.id.background).setBackground(color);
+    }
+
+    public void changeTextColorOnGreen(View view) {
+        Button b = (Button) findViewById(R.id.button_green);
+
+        Drawable color = (Drawable) b.getBackground();
+
+        findViewById(R.id.background).setBackground(color);
     }
 }
